f49cf59122f6883ff942a1259e0ed78d9663dc8e
ericg2 <l.ittlelettere@gmail.com>
2026-06-20 09:07:17 -0400

    stuf

--- crates/core/src/archiver.rs
+++ crates/core/src/archiver.rs
@@ -158,9 +158,7 @@ impl<'a, BE: DecryptFullBackend, I: ReadGlobalIndex> Archiver<'a, BE, I> {
                 }
                 Ok(ReadSourceEntry { path, node, open }) => {
                     let snapshot_path = if let Some(as_path) = as_path {
-                        as_path
-                            .clone()
-                            .join(path.strip_prefix(backup_path).unwrap_or(&Path::new("")))
+                        as_path.clone().join(path)
                     } else {
                         path
                     };
